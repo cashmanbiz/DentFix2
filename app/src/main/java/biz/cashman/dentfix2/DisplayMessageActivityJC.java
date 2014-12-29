a1d29698562760bbc3b7c3ac3281ad1595a37d7d
John Cashman <john@cashman.biz>
2014-12-29 23:58:51 +0000

    Change for second activity

--- app/src/main/java/biz/cashman/dentfix2/DisplayMessageActivityJC.java
+++ app/src/main/java/biz/cashman/dentfix2/DisplayMessageActivityJC.java
@@ -1,32 +1,35 @@
 package biz.cashman.dentfix2;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.content.Intent;
-import android.view.View;
-import android.widget.EditText;
+import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
-
-    public final static String EXTRA_MESSAGE="com.biz.cashman.dentfix2.MESSAGE";
+public class DisplayMessageActivityJC extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_display_message_activity_jc);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+        setContentView(textView);
     }
 
 
-    @Override
+/*    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_display_message_activity_jc, menu);
         return true;
     }
-
+*/
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -42,13 +45,5 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-/** Called when user clicks send button */
-    public void sendMessage(View view){
-        Intent intent=new Intent(this,DisplayMessageActivityJC.class);
-        EditText editText= (EditText) findViewById(R.id.edit_message);
-        String message =editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE,message);
-        startActivity(intent);
 
-    }
 }
