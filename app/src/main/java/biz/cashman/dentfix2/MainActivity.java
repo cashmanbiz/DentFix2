fad0396d37f3359e70c45849559958a9b5009c68
John Cashman <john@cashman.biz>
2015-01-01 16:46:17 +0000

    Change for action bar

--- app/src/main/java/biz/cashman/dentfix2/MainActivity.java
+++ app/src/main/java/biz/cashman/dentfix2/MainActivity.java
@@ -3,6 +3,7 @@ package biz.cashman.dentfix2;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.content.Intent;
 import android.view.View;
@@ -23,8 +24,22 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+
+       // getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu_main,menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    /** Called when user clicks send button */
+    public void sendMessage(View view){
+        Intent intent=new Intent(this,DisplayMessageActivityJC.class);
+        EditText editText= (EditText) findViewById(R.id.edit_message);
+        String message =editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE,message);
+        startActivity(intent);
     }
 
     @Override
@@ -32,23 +47,25 @@ public class MainActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        /* int id = item.getItemId(); */
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+     /*    if (id == R.id.action_settings) {
             return true;
         }
+       */
+        switch(item.getItemId()){
+            case(R.id.action_search):
+               //openSearch();
+                return true;
+            case(R.id.action_settings):
+        //        openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
 
-        return super.onOptionsItemSelected(item);
+        }
+       // return super.onOptionsItemSelected(item);
     }
 
-/** Called when user clicks send button */
-    public void sendMessage(View view){
-        Intent intent=new Intent(this,DisplayMessageActivityJC.class);
-        EditText editText= (EditText) findViewById(R.id.edit_message);
-        String message =editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE,message);
-        startActivity(intent);
-
-    }
 }
